14a6cfae3a6a195528d6855d35f972738fb309e2
YOUNGmaxer <517538470@qq.com>
2019-09-08 01:42:17 +0800

    修改一个文案bug

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,7 @@ const path = require('path');
 const resolve = dir => path.join(__dirname, dir);
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/quick-pay-deploy'
-    : '/',
+
   lintOnSave: true,
   chainWebpack: (config) => {
     config.resolve.alias
